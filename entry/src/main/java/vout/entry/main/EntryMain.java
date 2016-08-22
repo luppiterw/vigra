898ec26f055d9702c02fb66563b95087fefaf948
hughie <530125717@qq.com>
2016-08-22 22:21:04 +0800

    add entry codes

---
+++ entry/src/main/java/vout/entry/main/EntryMain.java
@@ -0,0 +1,10 @@
+package vout.entry.main;
+
+/**
+ * Created by hughie on 16/8/22.
+ */
+public class EntryMain {
+    public static void main(String args[]) {
+        System.out.println("[vout.entry.main] Hello! args=" + args);
+    }
+}
---
+++ kit/src/main/java/vout/kit/main/KitMain.java
@@ -0,0 +1,10 @@
+package vout.kit.main;
+
+/**
+ * Created by hughie on 16/8/22.
+ */
+public class KitMain {
+    public static void main(String args[]) {
+        System.out.println("[vout.kit.main] Hello! args=" + args);
+    }
+}
